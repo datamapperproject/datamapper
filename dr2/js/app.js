ebce9dbc41d1f22d2283ce7193c7136081459960
Neduecker <davidneudecker@gmail.com>
2023-05-27 12:54:34 +0200

    add dragging

--- dr2/js/app.js
+++ dr2/js/app.js
@@ -1,31 +1,31 @@
 //create array of objects 
 var proms = [
-  {name: "sense", type: "verb", column: 0, action:0, active : true, links : ["linkA", "linkB"], desc: "sence can be listened, seen, felt, smelt, tasted"},
-  {name: "lifeworlds", type: "subject", column:1, action:0, active : false, links : ["linkA"],desc: "lifeworlds are the worlds we live in"},
-  {name:"to describe", type:"verb2", column:2, action:0, active : false, links : ["linkB"], desc: "description is a way to understand"},
-  {name: "needs and aspirations", type:"object", column:3, action:0 , active :false, links : ["toolA"], desc: "needs and aspirations are the things we want"},
-  {name: "formulate", type: "verb", column: 0, action:1, active : true, links : ["linkC"], desc: "formulation is a way to understand"},
-  {name: "concepts", type: "subject", column:1, action:1, active : false, links : ["linkC"], desc: "concepts are the things we think"},
-  {name:"to frame", type:"verb2", column:2, action:1, active : false, links : ["linkD"], desc: "framing is a way to understand"},
-  {name: "problems", type:"object", column:3 , action:1, active : false, links : ["toolD"], desc: "problems are the things we want to change"},
-  {name: "gather", type: "verb", column: 0, action:2, active : true, links : ["linkE"], desc: "gathering is a way to understand"},
-  {name: "data", type: "subject", column:1, action:2, active : false, links : ["linkE"], desc: "data is the things we know"},
-  {name:"to analyse", type:"verb2", column:2, action:2, active : false, links : ["linkF", "toolB"], desc: "analysis is a way to understand"},
-  {name: "limits", type:"object", column:3, action:2, active : false , links : ["toolB", "linkK"], desc: "limits are the things we can't do"},
-  {name: "imagine", type: "verb", column: 0, action:3, active : true, links : ["linkG"], desc: "imagination is a way to understand"},
-  {name: "potentials", type: "subject", column:1, action:3, active : false, links : ["linkG"], desc: "potentials are the things we can do"},
-  {name:"to create", type:"verb2", column:2, action:3, active : false, links : ["linkB", "toolC"], desc: "creation is a way to understand"},
-  {name: "ideas", type:"object", column:3, action:3 , active : false, links : ["toolC", "linkA"], desc: "ideas are the things we can do"},
-  {name: "craft", type: "verb", column: 0, action:4, active : true, links : ["linkH"], desc: "craft is a way to understand"},
-  {name: "cases", type: "subject", column:1, action:4, active : false, links : ["linkH"], desc: "cases are the things we can do"},
-  {name:"to evaluate", type:"verb2", column:2, action:4, active : false, links : ["linkD", "toolD"], desc: "evaluation is a way to understand"},
-  {name: "solutions", type:"object", column:3 , action:4, active : false, links : ["toolA"], desc: "solutions are the things we can do"},
-  {name: "empower", type: "verb", column: 0, action:5, active : true, links : ["linkI"], desc: "empowerment is a way to understand"},
-  {name: "participants", type: "subject", column:1, action:5, active : false, links : ["linkI"], desc: "participants are the things we can do"},
-  {name:"to negotiate", type:"verb2", column:2, action:5, active : false, links : ["linkF", "toolA"], desc: "negotiation is a way to find compromises"},
-  {name: "projects", type:"object", column:3 , action:5, active : false, links : ["toolC"], desc: "projects are the things we can do"},
+  {name: "sense", type: "verb", column: 0, action:0, active : true, links : ["link A", "link B"], desc: "sence can be listened, seen, felt, smelt, tasted"},
+  {name: "lifeworlds", type: "subject", column:1, action:0, active : false, links : ["case A"],desc: "lifeworlds are the worlds we live in"},
+  {name:"to describe", type:"verb2", column:2, action:0, active : false, links : ["link B"], desc: "description is a way to understand"},
+  {name: "needs and aspirations", type:"object", column:3, action:0 , active :false, links : ["tool A"], desc: "needs and aspirations are the things we want"},
+  {name: "formulate", type: "verb", column: 0, action:1, active : true, links : ["case C"], desc: "formulation is a way to understand"},
+  {name: "concepts", type: "subject", column:1, action:1, active : false, links : ["link C"], desc: "concepts are the things we think"},
+  {name:"to frame", type:"verb2", column:2, action:1, active : false, links : ["lin D"], desc: "framing is a way to understand"},
+  {name: "problems", type:"object", column:3 , action:1, active : false, links : ["tool D"], desc: "problems are the things we want to change"},
+  {name: "gather", type: "verb", column: 0, action:2, active : true, links : ["link E"], desc: "gathering is a way to understand"},
+  {name: "data", type: "subject", column:1, action:2, active : false, links : ["case E"], desc: "data is the things we know"},
+  {name:"to analyse", type:"verb2", column:2, action:2, active : false, links : ["link F", "tool B"], desc: "analysis is a way to understand"},
+  {name: "limits", type:"object", column:3, action:2, active : false , links : ["tool B", "link K"], desc: "limits are the things we can't do"},
+  {name: "imagine", type: "verb", column: 0, action:3, active : true, links : ["link G"], desc: "imagination is a way to understand"},
+  {name: "potentials", type: "subject", column:1, action:3, active : false, links : ["link G"], desc: "potentials are the things we can do"},
+  {name:"to create", type:"verb2", column:2, action:3, active : false, links : ["link B", "tool C"], desc: "creation is a way to understand"},
+  {name: "ideas", type:"object", column:3, action:3 , active : false, links : ["tool C", "link A"], desc: "ideas are the things we can do"},
+  {name: "craft", type: "verb", column: 0, action:4, active : true, links : ["link H"], desc: "craft is a way to understand"},
+  {name: "cases", type: "subject", column:1, action:4, active : false, links : ["link H"], desc: "cases are the things we can do"},
+  {name:"to evaluate", type:"verb2", column:2, action:4, active : false, links : ["link D", "tool D"], desc: "evaluation is a way to understand"},
+  {name: "solutions", type:"object", column:3 , action:4, active : false, links : ["tool A"], desc: "solutions are the things we can do"},
+  {name: "empower", type: "verb", column: 0, action:5, active : true, links : ["link I"], desc: "empowerment is a way to understand"},
+  {name: "participants", type: "subject", column:1, action:5, active : false, links : ["link I"], desc: "participants are the things we can do"},
+  {name:"to negotiate", type:"verb2", column:2, action:5, active : false, links : ["link F", "tool A"], desc: "negotiation is a way to find compromises"},
+  {name: "projects", type:"object", column:3 , action:5, active : false, links : ["tool C"], desc: "projects are the things we can do"},
 ];
-
+var count=0;
 
 // assign svg to D3 selection
 var svg = d3.select("body")
@@ -48,17 +48,16 @@ const simulation = d3.forceSimulation()
 var linktemp = group.append('g');
 
 //create node  as circle for each element in array with d3
-var nodes = group.selectAll("rect")
+var nodes = group.selectAll("circle")
   .data(proms)
   .enter()
-  .append("rect")
-  .attr("width", 100)
-  .attr("height",25)
-  .attr("x", function(d) {
-    return d.column * 200 + 50;
+  .append("circle")
+  .attr("r", 30)
+  .attr("cx", function(d) {
+    return d.column * 220 + 50;
   })
-  .attr("y", function(d) {
-    return d.action * 30 + 50;   
+  .attr("cy", function(d) {
+    return d.action * 65 + 50;   
   })
   .style("fill", function(d) {
     return d.active ? "lightblue" : "white";
@@ -76,10 +75,10 @@ var labels = group.selectAll("text")
   .enter()
   .append("text")
   .attr("x", function(d) {
-    return d.column * 200 + 100;
+    return d.column * 220 + 50;
   })
   .attr("y", function(d) {
-    return d.action * 30 + 65;
+    return d.action * 65 + 52;
   })
   .text(function(d) {
     return d.name;
@@ -93,8 +92,6 @@ var labels = group.selectAll("text")
   .attr("pointer-events", "none")
   ;
 
-
-
 //create links between nodes
 var link;
 function createlink(t,data) {
@@ -104,46 +101,46 @@ function createlink(t,data) {
  if(link === undefined)   
  {
   link = linktemp.append("line")
-  .attr("x1", t.x.baseVal.value)
-  .attr("y1", t.y.baseVal.value)
-  .attr("x2", t.x.baseVal.value)
-  .attr("y2", t.y.baseVal.value)
+  .attr("x1", t.cx.baseVal.value)
+  .attr("y1", t.cy.baseVal.value)
+  .attr("x2", t.cx.baseVal.value)
+  .attr("y2", t.cy.baseVal.value)
   .attr("stroke-width",4)
   .attr("stroke", "black")
   .attr("opacity", 0.2)
   .attr("onclick", "this.remove()") //remove line on click
   ;
   reactivate(0,false);
-    reactivate(1,true);
+  reactivate(1,true);
+  crateTools(data, t.cx.baseVal.value, t.cy.baseVal.value);
  }
   else  if  ( data.column === 3)
-    {
-      console.log("end");
-      link.attr("x2", t.x.baseVal.value)
-          .attr("y2", t.y.baseVal.value);
-       link = undefined;
-       reactivate(3, false);
-       crateTools(data, t.x.baseVal.value, t.y.baseVal.value);
-    }
-    
-    else {
-
-    link.attr("x2", t.x.baseVal.value)
-        .attr("y2", t.y.baseVal.value);
-    link = undefined;
-
-    link = linktemp.append("line")
-    .attr("x1", t.x.baseVal.value)
-    .attr("y1", t.y.baseVal.value)
-    .attr("x2", t.x.baseVal.value)
-    .attr("y2", t.y.baseVal.value)
-    .attr("stroke-width",4)
-    .attr("stroke", "black")
-    .attr("opacity", 0.2)
-    .attr("onclick", "this.remove()") //remove line on click
-
-    reactivate(data.column,false);
-    reactivate(data.column +1,true);
+  {
+    console.log("end");
+ 
+    link.attr("x2", t.cx.baseVal.value)
+        .attr("y2", t.cy.baseVal.value);
+     link = undefined;
+     reactivate(3, false);
+     crateTools(data, t.cx.baseVal.value, t.cy.baseVal.value);
+     ++count;
+  } 
+  else {
+  link.attr("x2", t.cx.baseVal.value)
+      .attr("y2", t.cy.baseVal.value);
+  link = undefined;
+  link = linktemp.append("line")
+  .attr("x1", t.cx.baseVal.value)
+  .attr("y1", t.cy.baseVal.value)
+  .attr("x2", t.cx.baseVal.value)
+  .attr("y2", t.cy.baseVal.value)
+  .attr("stroke-width",4)
+  .attr("stroke", "black")
+  .attr("opacity", 0.2)
+  .attr("onclick", "this.remove()") //remove line on click
+  reactivate(data.column,false);
+  reactivate(data.column +1,true);
+  crateTools(data, t.cx.baseVal.value, t.cy.baseVal.value);
   }
   if(data != undefined && data.column === 3)
     reactivate(0,true);
@@ -169,39 +166,6 @@ document.onmousemove = function(e){
       .attr("y2", cursorY);
 }
 // update node active status base on order of sentence 
-document.onmousedown = function(e){
-
-
-  if(link === undefined) return;
-
-  var node = document.elementFromPoint( e.pageX, e.pageY);
-  var data = d3.select(node).data()[0];
-
-  if(node.tagName !== "rect" || !data.active) {
-    link.remove();
-    link = undefined;
-    reactivate(0,true);
-    reactivate(1, false);
-    reactivate(2, false);
-    reactivate(3, false);
-  }
-  else if  ( data.column === 3)
-  {
-    link.attr("x2", node.x.baseVal.value)
-        .attr("y2", node.y.baseVal.value);
-    link = undefined;
-    reactivate(3, false);
-    crateTools(data, node.x.baseVal.value, node.y.baseVal.value);
-  }
-  else {
-    link.attr("x2", node.x.baseVal.value)
-        .attr("y2", node.y.baseVal.value);
-    link = undefined;
-  }
-
-  if(data != undefined && data.column === 3)
-    reactivate(0,true);
-}
 
 function reactivate(c, t){
   nodes
@@ -221,39 +185,50 @@ var toolsText;
 function crateTools(data, x,y){
  // create array of object from data.links with index
   var arr = data.links.map(function(d, i) {
-    return {name: d, index: i, x: 800, y: i*70};
+    return {name: d, index: i, x: 0, y:0};
   });
 
   //create node  as circle for each element in array with d3
-  var x = Math.random()*1000;
-  var y = 500 + Math.random()*250;
-  tools=   group.append("g").selectAll("circle")
+  var tools = group.append("g").selectAll("rect")
     .data(arr)
-    .enter()
-    .append("circle")
-    .attr("r", 30)
-    .attr("cx", function(d) {return x + 100 * d.index;}) 
-    .attr("cy", function(d) {return y}) 
-    .style("fill", "lightblue")
+    .enter().append("g")
+    .call(function () {
+      return d3.drag().on('drag', function (d, i) {
+          d.x += d3.event.dx;
+          d.y += d3.event.dy;
+          d3.select(this).attr("transform", function () {
+              return "translate(" + [d.x, d.y] + ")";
+          });
+  
+      })
+  }())
+    ;
+    
+    tools.append("rect")
+    .attr("width", 100)
+    .attr("height", 50)
+    .attr("x", function(d){return x}) 
+    .attr("y", function(d){return 450+ count * 100+ 50 * d.index }) 
+    .style("fill", "lightgrey")
     .style("stroke", "black")
     .attr("z-index", "10")
+
     ;
 
-    //add labels to each nodes
-    toolsText = group.append("g").selectAll("text")
-    .data(arr)
-    .enter()
-    .append("text")
-    .attr("x", function(d) {return x+ 100 * d.index;}) 
-    .attr("y", function(d) {return y}) 
-    .text(function(d) {return d.name;})
-    .attr("font-family", "helvetica")
+   // add text to rect as child
+   tools.append("text")
+    .attr("x", function(d){return x+50})
+    .attr("y", function(d){return 450+ count * 100+ 50 * d.index +25;})
     .attr("font-size", "12px")
     .attr("fill", "black")
     .attr("text-anchor", "middle")
-    .attr("vertical-align", "middle")
-    .attr("z-index", "0")
+    .attr("alignment-baseline", "middle")
+    .attr("pointer-events", "none")
+    .attr("font-family", "helvetica")
+    .text( function(d) {return d.name;})
+    .style("pointer-events", "none")
     ;
+    
 }
 
 function ticked() {
@@ -274,4 +249,9 @@ svg.call(zoom.on("zoom", function() {
     toolsText.text( function(d) {return d.name;});
   }
 }));
+
+
+//implement drag and drop for tools
+
+
 
